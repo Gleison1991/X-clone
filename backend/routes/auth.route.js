b0ca7b0ca63b1f488a4c51a900a5bfcba3fdc057
gleison <gleisondeivison3@gmail.com>
2024-09-05 14:09:37 -0300

    3-Auth handled and routes protected in client

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/logout", logout);
+router.post("/logout", logout); // tava get
 
 export default router;
